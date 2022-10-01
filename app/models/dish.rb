70ad1aff670e998daa20f6d6a7709a3e0a82299e
cesar214567 <camgcamg11@gmail.com>
2022-10-01 13:48:48 -0500

    changing link

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,6 @@
 class Dish < ApplicationRecord
   belongs_to :restaurant
-  has_many :dish_ingredients,dependent: :delete_all
+  has_many :dish_ingredients,dependent: :destroy
   has_many :ingredients, through: :dish_ingredients
 
   has_one_attached :image
